404dc260e2ecb51a380ef1ed40e0469adf32eff5
Devessier <baptiste@devessier.fr>
2021-10-01 21:06:07 +0200

    build: set up yarn workspaces and scaffold temporal package

---
+++ packages/temporal/src/workflows/index.ts
@@ -0,0 +1,17 @@
+import { createActivityHandle } from '@temporalio/workflow';
+import { Example } from '../interfaces';
+// Only import the activity types
+import type * as activities from '../activities';
+
+const { greet } = createActivityHandle<typeof activities>({
+  startToCloseTimeout: '1 minute',
+});
+
+/** A workflow that simply calls an activity */
+export const example: Example = (name: string) => {
+  return {
+    async execute(): Promise<string> {
+      return await greet(name);
+    },
+  };
+};
