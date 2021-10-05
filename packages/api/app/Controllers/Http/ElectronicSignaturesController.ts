8b4012d79ac0883b88ed3491c2273f39af44914b
Devessier <baptiste@devessier.fr>
2021-10-05 19:37:41 +0200

    feat(api): generate ourselves the name of the uploaded file and return it

--- packages/api/app/Controllers/Http/ElectronicSignaturesController.ts
+++ packages/api/app/Controllers/Http/ElectronicSignaturesController.ts
@@ -1,7 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema } from '@ioc:Adonis/Core/Validator'
+import { cuid } from '@ioc:Adonis/Core/Helpers'
+
 export default class ElectronicSignaturesController {
-  public async create({ request }: HttpContextContract): Promise<void> {
+  public async create({ request }: HttpContextContract): Promise<{
+    procedureUuid: string
+    documentURL: string
+  }> {
     const createProcedureSchema = schema.create({
       document: schema.file({
         size: '10mb',
@@ -10,8 +15,15 @@ export default class ElectronicSignaturesController {
     })
     const { document } = await request.validate({ schema: createProcedureSchema })
 
+    const documentName = `${cuid()}.pdf`
     await document.moveToDisk('./', {
+      name: documentName,
       contentType: 'application/pdf',
     })
+
+    return {
+      documentURL: documentName,
+      procedureUuid: cuid(),
+    }
   }
 }
