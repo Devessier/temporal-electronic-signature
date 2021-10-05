208c808d5b4c5af1936bc931cb097d1162551850
Devessier <baptiste@devessier.fr>
2021-10-06 00:38:36 +0200

    feat(api): create workflow when document has been uploaded to S3 bucket

--- packages/api/app/Controllers/Http/ElectronicSignaturesController.ts
+++ packages/api/app/Controllers/Http/ElectronicSignaturesController.ts
@@ -1,6 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { schema } from '@ioc:Adonis/Core/Validator'
 import { cuid } from '@ioc:Adonis/Core/Helpers'
+import { Connection, WorkflowClient } from '@temporalio/client'
+import { ElectronicSignature } from '@temporal-electronic-signature/temporal/lib/interfaces'
 
 export default class ElectronicSignaturesController {
   public async create({ request }: HttpContextContract): Promise<{
@@ -21,9 +23,17 @@ export default class ElectronicSignaturesController {
       contentType: 'application/pdf',
     })
 
+    const connection = new Connection()
+    const client = new WorkflowClient(connection.service)
+    const handle = client.createWorkflowHandle<ElectronicSignature>('electronicSignature', {
+      taskQueue: 'electronic-signature',
+    })
+    await handle.start() // kick off the purchase async
+
     return {
       documentURL: documentName,
-      procedureUuid: cuid(),
+      procedureUuid: handle.workflowId,
     }
+  }
   }
 }
