88657dd4848a56bacee45632b5c10ccbe19157e1
Devessier <baptiste@devessier.fr>
2021-10-07 18:49:29 +0200

    chore(api): remove minio bucket creation

--- packages/api/providers/AppProvider.ts
+++ packages/api/providers/AppProvider.ts
@@ -1,5 +1,4 @@
 import { ApplicationContract } from '@ioc:Adonis/Core/Application'
-import Env from '@ioc:Adonis/Core/Env'
 
 export default class AppProvider {
   constructor(protected app: ApplicationContract) {}
@@ -8,26 +7,7 @@ export default class AppProvider {
     // Register your own bindings
   }
 
-  public async boot() {
-    const Minio = await import('minio')
-
-    const client = new Minio.Client({
-      endPoint: 'localhost',
-      useSSL: false,
-      port: 9100,
-      accessKey: Env.get('S3_KEY'),
-      secretKey: Env.get('S3_SECRET'),
-    })
-
-    const bucketExists = await client.bucketExists('documents')
-    if (bucketExists === true) {
-      return
-    }
-
-    await client.makeBucket('documents', 'fr-FR')
-
-    // client.setBucketPolicy('documents', )
-  }
+  public async boot() {}
 
   public async ready() {
     // App is ready
