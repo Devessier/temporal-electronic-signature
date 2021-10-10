a8fbe4bb303bb4a73efd4c77151303bf88bd433a
Devessier <baptiste@devessier.fr>
2021-10-10 21:06:42 +0200

    fix(app): make create route the index page

--- packages/app/src/routes/__layout.svelte
+++ packages/app/src/routes/__layout.svelte
@@ -41,7 +41,7 @@
 				</a>
 
 				<a
-					href="/create"
+					href="/"
 					class="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
 				>
 					Create
