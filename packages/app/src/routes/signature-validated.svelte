133ccbcad441aa804850bfdb7f74321f8da18cbf
Devessier <baptiste@devessier.fr>
2021-10-10 20:05:23 +0200

    feat(app): show stamped document on signature validated page

--- packages/app/src/routes/signature-validated.svelte
+++ packages/app/src/routes/signature-validated.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
 	import AppLayout from '$lib/AppLayout.svelte';
+	import { useAppContext } from '$lib/contexts/app';
+	import PdfViewer from '$lib/PdfViewer.svelte';
+
+	const { state } = useAppContext();
+
+	$: documentURL = $state.context.documentPresignedURL;
 </script>
 
 <AppLayout title="Signature validated">
-	<div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+	<div class="mt-2 flex flex-col flex-grow">
+		{#if documentURL}
+			<div class="flex-grow grid grid-cols-1 grid-rows-1">
+				<PdfViewer url={documentURL} title="Signed document" />
+			</div>
+		{/if}
+	</div>
 </AppLayout>
